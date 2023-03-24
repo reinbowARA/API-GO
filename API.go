921897d36800dc60cc04aa27a25164c8fc06e063
Roman_Ablyazov <reinbowARA@yandex.ru>
2023-03-24 22:22:53 +0300

    recreate API

--- API.go
+++ API.go
@@ -1,85 +1,30 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
-type CurrencyListResponse struct {
-	Status  string   `json:"status"`
-	Message string   `json:"message"`
-	Data    []string `json:"data"`
-}
-
-type CurrencyRange struct {
-	Status  int               `json:"status"`
-	Message string            `json:"message"`
-	Data    map[string]string `json:"data"`
-}
-
 func main() {
-	apiKey := "49293d12f95c00b54dab08a4274390d7"
-	url := fmt.Sprintf("https://currate.ru/api/?get=currency_list&key=%s", apiKey)
-	resp, err := http.Get(url)
+	apikey := "Xa0PYkfM35Sz02AFPkjPRXCIDKb2MXJj"
+	apiURL := "https://api.apilayer.com/fixer/timeseries?base=USD&symbols=RUB&start_date=2023-03-01&end_date=2023-03-05"
+
+	req, err := http.NewRequest("GET", apiURL, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("apikey", apikey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
 
-	var currencyListResponse CurrencyListResponse
-	err = json.Unmarshal(body, &currencyListResponse)
-	if err != nil {
-		panic(err)
-	}
-
-	if currencyListResponse.Status != "200" {
-		panic(currencyListResponse.Message)
-	}
-
-	var pairs [92]string
-
-	//получаем количество пар
-	for value, pair := range currencyListResponse.Data {
-		fmt.Println(value, " = ", string(pair))
-		pairs[value] = string(pair)
-	}
-
-	currentDUO := strings.Join(pairs[:], ",")
-
-	url = fmt.Sprintf("https://currate.ru/api/?get=rates&pairs=%s&key=%s", currentDUO, apiKey)
-
-	resp, err = http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	var currencyRange CurrencyRange
-
-	err = json.Unmarshal(body, &currencyRange)
-	if err != nil {
-		panic(err)
-	}
-
-	if currencyRange.Status != 200 {
-		panic(currencyListResponse.Message)
-	}
-
-	//получаем количество пар с расценкой на сегодняшний день (по умолчанию берутся последние данные (GMT +03:00))
-	for pair, rate := range currencyRange.Data {
-		fmt.Println(string(pair), "=", string(rate))
-	}
+	fmt.Println(string(body))
 }

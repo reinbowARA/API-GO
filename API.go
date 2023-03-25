33aeac9fd937affe398a1341a467faccdae0e029
Roman_Ablyazov <reinbowARA@yandex.ru>
2023-03-25 17:59:05 +0300

    try add BD

--- API.go
+++ API.go
@@ -1,27 +1,73 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type APIkey struct {
+	Key string `json:"key"`
+}
+
 func main() {
-	apikey := "Xa0PYkfM35Sz02AFPkjPRXCIDKb2MXJj"
+	file, err := os.Open("apikey.json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
-	var today string
-	today = time.Now().Format("2006-01-02")
-	fmt.Println(today)
+	var key APIkey
+	Key := json.NewDecoder(file)
+	err = Key.Decode(&key)
+	if err != nil {
+		panic(err)
+	}
 
-	apiURL := fmt.Sprintf("https://api.apilayer.com/fixer/%s?base=USD&symbols=RUB", today)
+	apiKey := key.Key // получаем api из json файла {"key":"token_API"}
 
+	//ticker := time.NewTicker(time.Minute * 10)
+
+	//for range ticker.C {
+	rate, err := getExchangeRate(apiKey)
+	if err != nil {
+		log.Println(err)
+		//continue
+	}
+	fmt.Println(rate)
+	err = saveExchangeRate(rate)
+	if err != nil {
+		log.Println(err)
+		//continue
+	}
+	log.Printf("Exchange rate saved: %f", rate)
+	//}
+}
+
+func getExchangeRate(apiKey string) (float64, error) {
+
+	type Rates struct {
+		RUB float64 `json:"RUB"`
+	}
+	type Response struct {
+		Rates Rates `json:"rates"`
+	}
+
+	apiURL := fmt.Sprintf("https://api.apilayer.com/fixer/latest?base=USD&symbols=RUB")
 	req, err := http.NewRequest("GET", apiURL, nil)
 	if err != nil {
 		panic(err)
 	}
 
-	req.Header.Set("apikey", apikey)
+	req.Header.Set("apikey", apiKey)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -31,6 +77,29 @@ func main() {
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println(string(body))
+	var date Response
+	err = json.Unmarshal(body, &date)
+	return date.Rates.RUB, err
+}
+
+func saveExchangeRate(rate float64) error {
+	db, err := sql.Open("sqlite3", "Current.db")
+	if err != nil {
+		return err
+	}
+	fmt.Println("Подключение с БД установленно!")
+	defer db.Close()
+	stmt, err := db.Prepare("insert into Current.exchange_rates (currency, rate, created_at) values ($1, $2, $3)")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec("USD", rate, time.Now())
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,3 +2,11 @@ module example.com/m/v2
 
 go 1.20
 
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/parnurzeal/gorequest v0.2.16 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	moul.io/http2curl v1.0.0 // indirect
+)

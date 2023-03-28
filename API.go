9d31d3f255b75ae214fa9a843b701c7278b54598
redARA <roma.ablyazoff@yandex.ru>
2023-03-28 17:57:41 +0300

    add func creat graph

--- API.go
+++ API.go
@@ -11,13 +11,16 @@ import (
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 type exchange_rates struct {
 	id         int
 	currency   string
-	rate       float32
-	created_at string
+	rate       float64
+	created_at time.Time
 }
 type APIkey struct {
 	Key string `json:"key"`
@@ -60,9 +63,7 @@ func main() {
 		//continue
 	}
 	log.Printf("Exchange rate saved: %f", rate)
-	for _, Cur := range Curr {
-		fmt.Println(Cur.id, Cur.currency, Cur.rate, Cur.created_at)
-	}
+	GetPngGraph(Curr)
 	//}
 }
 
@@ -128,9 +129,65 @@ func saveExchangeRate(rate float64) ([]exchange_rates, error) {
 		err := rows.Scan(&Cur.id, &Cur.currency, &Cur.rate, &Cur.created_at)
 		if err != nil {
 			panic(err)
-			continue
+			//continue
 		}
 		currenct = append(currenct, Cur)
 	}
 	return currenct, nil
+}
+func GetPngGraph(currents []exchange_rates) {
+	// Создание нового графика
+	p := plot.New()
+
+	// Установка заголовка графика
+	p.Title.Text = "USD to RUB Exchange Rate"
+
+	// Установка меток на осях координат
+	p.X.Label.Text = "Date"
+	p.Y.Label.Text = "Exchange Rate"
+
+	var CreatedAt []time.Time
+	for _, Currate := range currents {
+		Cur := Currate.created_at
+		CreatedAt = append(CreatedAt, Cur)
+	}
+
+	/*var times []time.Time
+	for _, Cur := range CreatedAt {
+		t, _ := time.Parse("2006-01-02", Cur)
+		times = append(times, t)
+	}*/
+
+	var rate []float64
+	for _, Currate := range currents {
+		Cur := Currate.rate
+		rate = append(rate, Cur)
+	}
+
+	// Создание точек для графика
+	pts := make(plotter.XYs, len(rate), len(CreatedAt))
+	for i := range pts {
+		pts[i].Y = rate[i]
+
+	}
+
+	for i := range pts {
+		pts[i].X = float64(CreatedAt[i].Unix())
+	}
+	// Создание линии для графика
+	line, err := plotter.NewLine(pts)
+	if err != nil {
+		panic(err)
+	}
+
+	// Добавление линии на график
+	p.Add(line)
+
+	// Устанавливаем метки на оси X
+	p.X.Tick.Marker = plot.TimeTicks{Format: "2006-01-02"}
+
+	// Сохранение графика в файл
+	if err := p.Save(20*vg.Inch, 20*vg.Inch, "src/usd_to_rub.png"); err != nil {
+		panic(err)
+	}
 }

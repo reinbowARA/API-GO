74f42ba31c938eadc2e2fb8e9158406bc409928e
redARA <roma.ablyazoff@yandex.ru>
2023-03-30 14:22:39 +0300

    add information in console

--- API.go
+++ API.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"time"
@@ -13,6 +14,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 )
 
@@ -42,29 +44,30 @@ func main() {
 
 	apiKey := key.Key // получаем api из json файла {"key":"token_API"}
 
-	//ticker := time.NewTicker(time.Minute * 1)
+	fmt.Println("Start program!")
+	fmt.Printf("Please wait %d min \n", int(math.Abs(30.0-float64(time.Now().Minute()))))
 
-	/*for {
-	log.Println("Start iteration")
-	if time.Now().Hour() != 0 && time.Now().Minute() != 0 {
-		time.Sleep(1 * time.Second)
-		continue
-	}*/
-	rate, err := getExchangeRate(apiKey)
-	if err != nil {
-		log.Println(err)
-		//continue
-	}
-	fmt.Println(rate)
+	for {
+		if time.Now().Minute() == 0 || time.Now().Minute() == 30 && time.Now().Second() == 0 {
+			log.Println("Start read API")
 
-	Curr, err := saveExchangeRate(rate)
-	if err != nil {
-		log.Println(err)
-		//continue
+			rate, err := getExchangeRate(apiKey)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			log.Println("API read successfully")
+			Curr, err := saveExchangeRate(rate)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			log.Printf("Exchange rate saved: %f", rate)
+			GetPngGraph(Curr)
+			log.Println("stand 30 minutes")
+		}
+
 	}
-	log.Printf("Exchange rate saved: %f", rate)
-	GetPngGraph(Curr)
-	//}
 }
 
 func getExchangeRate(apiKey string) (float64, error) {
@@ -106,13 +109,13 @@ func saveExchangeRate(rate float64) ([]exchange_rates, error) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Подключение с БД установленно!")
+	log.Println("Подключение с БД установленно!")
 	defer db.Close()
 	stmt, err := db.Prepare("insert into exchange_rates (currency, rate, created_at) values (?, ?, ?)")
 	if err != nil {
 		panic(err)
 	}
-	_, err = stmt.Exec("USD", rate, time.Now().Format("2006-01-02"))
+	_, err = stmt.Exec("USD", rate, time.Now().Format("2006-01-02 15:04:05"))
 	if err != nil {
 		panic(err)
 	}
@@ -129,7 +132,6 @@ func saveExchangeRate(rate float64) ([]exchange_rates, error) {
 		err := rows.Scan(&Cur.id, &Cur.currency, &Cur.rate, &Cur.created_at)
 		if err != nil {
 			panic(err)
-			//continue
 		}
 		currenct = append(currenct, Cur)
 	}
@@ -151,12 +153,6 @@ func GetPngGraph(currents []exchange_rates) {
 		Cur := Currate.created_at
 		CreatedAt = append(CreatedAt, Cur)
 	}
-
-	/*var times []time.Time
-	for _, Cur := range CreatedAt {
-		t, _ := time.Parse("2006-01-02", Cur)
-		times = append(times, t)
-	}*/
 
 	var rate []float64
 	for _, Currate := range currents {
@@ -183,11 +179,19 @@ func GetPngGraph(currents []exchange_rates) {
 	// Добавление линии на график
 	p.Add(line)
 
+	point, err := plotter.NewScatter(pts)
+	if err != nil {
+		panic(err)
+	}
+	// устанавливаем точки на красный цвет
+	point.GlyphStyle.Color = plotutil.Color(0)
+
+	p.Add(point)
 	// Устанавливаем метки на оси X
-	p.X.Tick.Marker = plot.TimeTicks{Format: "2006-01-02"}
+	p.X.Tick.Marker = plot.TimeTicks{Format: "2006-01-02 15:04:05"}
 
 	// Сохранение графика в файл
-	if err := p.Save(20*vg.Inch, 20*vg.Inch, "src/usd_to_rub.png"); err != nil {
+	if err := p.Save(10*vg.Inch, 10*vg.Inch, "src/usd_to_rub.png"); err != nil {
 		panic(err)
 	}
 }
